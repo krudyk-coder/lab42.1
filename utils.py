6677231fb0b87697ecb6b5d80c490527bdf5382f
Nazarchik <sorochenko.n.s@gmail.com>
2026-02-27 14:15:57 +0200

    prime com

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def get_fact(x):
 
 def nsd(x,y):
     return math.gcd(x, y)
+
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
+         if n % i == 0:
+             return False
+    return True
